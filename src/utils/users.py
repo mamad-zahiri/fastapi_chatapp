bdbfdf7f094f2ec4a4fcc2193e845df622fd6c38
Mohamad Zahiri <mamadzhri@gamil.com>
2024-07-22 18:09:14 +0330

    feat(users): add find_user and verify_password utils

--- src/utils/users.py
+++ src/utils/users.py
@@ -20,8 +20,29 @@ async def create_user(user: User) -> InsertOneResult:
     return await collection.insert_one(user.model_dump())
 
 
+async def find_user(email: str) -> User | None:
+    collection = get_collection("user")
+    restult = await collection.find_one({"email": email})
+
+    if restult is None:
+        return None
+
+    return User(
+        first_name=restult["first_name"],
+        last_name=restult["last_name"],
+        email=restult["email"],
+        password=restult["password"],
+        last_seen=restult["last_seen"],
+        email_verified=restult["email_verified"],
+    )
+
+
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
     return password_context.hash(password)
+
+
+def verify_password(raw_pass: str, hashed_pass: str) -> bool:
+    return password_context.verify(raw_pass, hashed_pass)
