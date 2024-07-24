bcc00072b9af78503f3fb1a8a4f64e5dc2cd46fc
Mohamad Zahiri <mamadzhri@gamil.com>
2024-07-24 17:55:01 +0330

    refactor(chat): improve chat server connection safety

--- src/chat/events.py
+++ src/chat/events.py
@@ -2,22 +2,53 @@ import socketio
 from socketio.exceptions import ConnectionRefusedError
 
 from src.auth.services import verify_token_service
+from src.settings import settings
 from src.utils.chat import token_provided
+from src.utils.jwt import decode_jwt
+from src.utils.users import find_user
 
 sio = socketio.AsyncServer(cors_allowed_origins="*", async_mode="asgi")
+
+# TODO: use redis as cache server
+#   It is not a good practice to save online users in dictionary. Instead
+#   we could save them in a Redis Cache or something.
+online_users = {}
 
 
 @sio.on("connect")
 async def connect(sid, e, auth):
+    # TODO: refactor and clean this function
     if not token_provided(auth):
         raise ConnectionRefusedError()
 
-    if not verify_token_service(auth["token"]):
+    token = auth["token"]
+
+    if not verify_token_service(token):
         raise ConnectionRefusedError()
+
+    decoded_token = decode_jwt(
+        token,
+        settings.jwt_access_secret_key,
+        settings.jwt_algorithm,
+    )
+
+    user = await find_user(decoded_token["email"])
+
+    if user is None:
+        raise ConnectionRefusedError()
+
+    user = user.model_dump(mode="json", exclude=["password", "email_verified"])
+    online_users.update({sid: user})
 
     await sio.emit("/auth/verify", "verified", to=sid)
 
 
 @sio.on("disconnect")
 async def disconnect(sid):
-    await sio.emit("broadcast", data=f"user {sid} disconnected")
+    # TODO: refactor and clean this function
+    try:
+        online_users.pop(sid)
+    except KeyError as e:
+        pass
+
+    await sio.emit("/broadcast/user-diconnect", data=sid)
