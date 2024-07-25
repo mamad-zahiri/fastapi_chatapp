70c7b7bfbaa939ecb85d907b3cafe310d78f8fb5
Mohamad Zahiri <mamadzhri@gamil.com>
2024-07-25 12:00:16 +0330

    feat(private chat): create private chat service
    now users can chat to each other using other users email

--- src/chat/events.py
+++ src/chat/events.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
 import socketio
 from socketio.exceptions import ConnectionRefusedError
 
 from src.auth.services import verify_token_service
+from src.chat.models import PrivateChat
 from src.settings import settings
+from src.users.models import User
 from src.utils.chat import token_provided
 from src.utils.jwt import decode_jwt
 from src.utils.users import find_user, get_all_users
@@ -13,6 +17,15 @@ sio = socketio.AsyncServer(cors_allowed_origins="*", async_mode="asgi")
 #   It is not a good practice to save online users in dictionary. Instead
 #   we could save them in a Redis Cache or something.
 online_users = {}
+
+
+async def get_receiver_and_status(email: str) -> tuple[User | dict, bool]:
+    user = online_users.get(email)
+
+    if user is None:
+        return await User.find_one(User.email == email), False
+
+    return user, True
 
 
 @sio.on("connect")
@@ -88,3 +101,33 @@ async def system_list_users(sid):
 async def system_list_online_users(sid):
     # TODO: refactor and clean this function
     return online_users
+
+
+@sio.on("/private/chat")
+async def private_chat(sid, env):
+    decoded_token = decode_jwt(
+        env["token"],
+        settings.jwt_access_secret_key,
+        settings.jwt_algorithm,
+    )
+    sender = await User.find_one(User.email == decoded_token["email"])
+
+    receiver, is_online = await get_receiver_and_status(env["receiver"])
+
+    if receiver is None:
+        return "invalid receiver"
+
+    payload = PrivateChat(
+        timestamp=datetime.now(),
+        message=env["message"],
+        file="",
+        sender=sender,
+    )
+
+    if is_online:
+        await sio.emit("/private/chat", payload.model_dump_json(exclude=["sender"]))
+
+    receiver.chats.append(payload)
+    await receiver.save()
+
+    return payload.model_dump_json(exclude=["sender"])
