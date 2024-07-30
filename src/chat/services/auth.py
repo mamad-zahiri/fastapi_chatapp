a7900d58bd5331ade08fa21371b2096f0323b108
Mohamad Zahiri <mamadzhri@gamil.com>
2024-07-30 19:18:43 +0330

    refactor: move verify user to service layer

--- src/chat/services/auth.py
+++ src/chat/services/auth.py
@@ -4,9 +4,10 @@ from socketio.exceptions import ConnectionRefusedError
 
 from src.auth.services import verify_token_service
 from src.chat.services.clients import online_users
+from src.db.models import User
 from src.settings import settings
 from src.utils.chat import token_provided
-from src.utils.jwt import decode_jwt
+from src.utils.jwt import decode_jwt, token_expired
 from src.utils.users import find_user
 
 
@@ -40,3 +41,16 @@ async def disconnection_service(sio: AsyncServer, sid) -> EmailStr:
     email = (await sio.get_session(sid)).get("email")
     online_users.pop(email)
     return email
+
+
+async def verify_user_service(env: dict[str, str]) -> User | None:
+    decoded_token = decode_jwt(
+        env["token"],
+        settings.jwt_access_secret_key,
+        settings.jwt_algorithm,
+    )
+
+    if decoded_token is None or token_expired(decoded_token):
+        return None
+
+    return await User.find_one(User.email == decoded_token["email"])
